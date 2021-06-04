91e90c474562ab73929c146b373fb07ccad1c106
Harold Dyson <hdyson@gmail.com>
2021-06-04 22:34:01 +0100

    Updated for reactive keyboard LEDs using RGB matrix

--- keyboards/crkbd/keymaps/hdyson/rules.mk
+++ keyboards/crkbd/keymaps/hdyson/rules.mk
@@ -1,6 +1,6 @@
 OLED_DRIVER_ENABLE  = yes
-RGBLIGHT_ENABLE = yes
-RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
 CFLAGS += -flto
 TAP_DANCE_ENABLE = yes
 EXTRAKEY_ENABLE = yes          # Audio control and System control
